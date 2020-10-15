b5c82c5c7f732547ba8b62c872daf08cda19a22a
unknown <erikalves0799@gmail.com>
2020-10-15 18:22:26 -0300

    First commit

---
+++ src/test/java/com/estudo/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
